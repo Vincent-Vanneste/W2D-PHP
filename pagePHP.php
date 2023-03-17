f6117eaeddac86bf887da62a9e5a8ca3acf1cb01
Vincent-Vanneste <vincent.vanneste1970@gmail.com>
2023-03-17 16:42:30 +0100

    ajout de la page show + modif html,css,js,php

--- pagePHP.php
+++ pagePHP.php
@@ -5,29 +5,5 @@ $page_titre = "Projet PHP";
 $page_description = "Ce projet permet de découvrir la programmation PHP.";
 
 $langage = $listeLangages[3];
-?>
-<!DOCTYPE html>
-<html lang="fr">
 
-<?php require 'head.php'; ?>
-
-<body>
-    <?php require 'header.php'; ?>
-
-    <main>
-        <section class="row p-3">
-            <figure class="col-2">
-                <img src="images/<?= $langage['logo']; ?>" class="figure-img" alt="...">
-                <figcaption class="figure-caption">Logo <?= $langage['titre']; ?></figcaption>
-            </figure>
-            <div class="col-8">
-                <h2><?= $langage['titre']; ?></h2>
-                <p><?= $langage['resume']; ?></p>
-        </section>
-    </main>
-
-    <?php require 'footer.php'; ?>
-
-</body>
-
-</html>
+require 'afficher.php';
